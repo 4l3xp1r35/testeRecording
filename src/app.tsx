f80bd46507bd95ddd0d5522acddc3801f138c2f5
4l3xp1r35 <alexandrepvg33@gmail.com>
2024-02-20 16:23:20 +0000

    first test for speech recording

---
+++ src/app.tsx
@@ -0,0 +1,37 @@
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+
+export function App() {
+  const { transcript, listening, resetTranscript, browserSupportsSpeechRecognition, isMicrophoneAvailable } = useSpeechRecognition();
+
+  const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'pt-PT' });
+  const stopListening = () => SpeechRecognition.stopListening();
+
+  const record = () => {
+    if (!browserSupportsSpeechRecognition) {
+      alert('O navegador não suporta reconhecimento de voz.');
+      return;
+    }
+
+    if (!isMicrophoneAvailable) {
+      alert('Microfone não disponível.');
+      return;
+    }
+    console.log("deu")
+    startListening();
+  };
+
+  const stopAndReset = () => {
+    stopListening();
+    resetTranscript();
+  };
+
+  return (
+    <div className='mx-auto max-w-6xl my-12 space-y-6 px-5'>
+      <h2 className="text-3xl">Texto 1</h2>
+      <p>Microfone: {listening ? 'ligado' : 'desligado'}</p>
+      <span>{transcript}</span>
+      <button onClick={record} className="bg-blue-500 h-15 w-1/2 text-2xl ms-3">Gravar</button>
+      <button onClick={stopAndReset} className="bg-red-500 h-15 w-1/2 text-2xl ms-3">Parar de gravar</button>
+    </div>
+  )
+}
